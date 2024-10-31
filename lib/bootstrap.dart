f4a80184720326bf062036bb77c21cba5f25cf14
Benjamin <s.benjamin@live.fr>
2024-10-31 13:34:14 +0100

    Rollback to .env usage. Vercel is not supporting well Flutter

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,8 +1,8 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:ez_fit_app/src/utils/constants/ez_const_string.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -17,9 +17,12 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   await Hive.openBox<String?>(EzConstString.hiveId);
   await Hive.openBox<String>(EzConstString.hiveUserSettings);
 
+  // Load .env
+  await dotenv.load();
+
   // Initialize Supabase
-  final supabaseUrl = Platform.environment['SUPABASE_URL'];
-  final supabaseAnonKey = Platform.environment['SUPABASE_ANON_KEY'];
+  final supabaseUrl = dotenv.env['SUPABASE_URL'];
+  final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'];
   if (supabaseUrl == null || supabaseAnonKey == null) {
     throw Exception(
       'SUPABASE_URL and/or SUPABASE_ANON_KEY environment variable is missing',
