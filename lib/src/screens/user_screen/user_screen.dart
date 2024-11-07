26cb140a3d539477631d9c11530e724428018a50
Benjamin <s.benjamin@live.fr>
2024-11-07 08:54:57 +0100

    Add comma

--- lib/src/screens/user_screen/user_screen.dart
+++ lib/src/screens/user_screen/user_screen.dart
@@ -3,12 +3,12 @@ import 'package:ez_fit_app/src/shared/ez_scaffold_body/ez_scaffold_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-/// User screen for "/users/:id" route.
 class UserScreen extends ConsumerWidget {
-  /// User screen for "/users/:id" route.
-  const UserScreen({super.key, required this.id});
+  const UserScreen({
+    super.key,
+    required this.id,
+  });
 
-  /// The user to display.
   final String id;
 
   @override
