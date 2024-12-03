dfc77d4e193c2caac71485064ca0bc693b9cff6c
Benjamin <s.benjamin@live.fr>
2024-12-03 15:52:24 +0100

    Fix loading on custom notes

--- lib/src/features/workout_exercise/form_widget/workout_exercise_form_custom_note.dart
+++ lib/src/features/workout_exercise/form_widget/workout_exercise_form_custom_note.dart
@@ -4,7 +4,6 @@ import 'package:ez_fit_app/src/shared/ez_text_form_field/ez_text_form_field.dart
 import 'package:ez_fit_app/src/utils/extension/widget_ref_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:skeletonizer/skeletonizer.dart';
 
 class WorkoutExerciseFormCustomNotes extends ConsumerWidget {
   const WorkoutExerciseFormCustomNotes({
@@ -20,12 +19,12 @@ class WorkoutExerciseFormCustomNotes extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final customNotes = ref.watch(
       workoutFormControllerProvider(workoutId: workoutId).select(
-            (value) {
+        (value) {
           return value.when(
             data: (v) => v.workoutExercises
                 .firstWhere(
                   (e) => e.id == workoutExerciseId,
-            )
+                )
                 .customNotes,
             error: (error, stackTrace) => throw Exception(error.toString()),
             loading: () => null,
@@ -37,25 +36,17 @@ class WorkoutExerciseFormCustomNotes extends ConsumerWidget {
     return EzFormItemLayout(
       itemLabel: ref.loc.workoutExerciseFormCustomNotes,
       itemDescription: ref.loc.workoutExerciseFormCustomNotesDescription,
-      child: customNotes != null
-          ? EzTextFormField(
+      child: EzTextFormField(
         initialValue: customNotes,
         hintText: ref.loc.workoutExerciseFormCustomNotesHint,
         maxLines: 3,
         onChanged: (value) {
           ref
               .read(
-            workoutFormControllerProvider(workoutId: workoutId)
-                .notifier,
-          )
+                workoutFormControllerProvider(workoutId: workoutId).notifier,
+              )
               .updateCustomNotes(workoutExerciseId, value);
         },
-      )
-          : Skeletonizer(
-        child: EzTextFormField(
-          hintText: ref.loc.workoutExerciseFormCustomNotesHint,
-          maxLines: 3,
-        ),
       ),
     );
   }
